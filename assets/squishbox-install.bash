e641df07609f8959a69b8c8eb51dcc4e961d87b1
Bill Peterson <albedozero@gmail.com>
2024-04-21 01:03:12 -0500

    Update squishbox-install.bash

--- assets/squishbox-install.bash
+++ assets/squishbox-install.bash
@@ -1,2 +1,15 @@
 #!/bin/bash
-curl -sL geekfunklabs.com/squishbox | bash
+
+inform() {
+  echo -e "$(tput setaf 6)$1$(tput sgr0)"
+}
+warning() {
+  echo -e "$(tput setaf 3)$1$(tput sgr0)"
+}
+
+sleep 1
+
+warning "This link is outdated."
+inform "To install the active version of the squishbox software, use the command:"
+echo "curl -sL geekfunklabs.com/squishbox | bash"
+exit
